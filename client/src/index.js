8cb66a6dc759f08a881c53c2d0f4650450d1bfda
nasir12ahmad <nasirnawruzi@gmail.com>
2022-11-26 19:28:28 +0700

    completed client and sever side

---
+++ client/src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+if (localStorage.zomatoUser) {
+  const { token } = JSON.parse(localStorage.zomatoUser);
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+//it should be installed on client folder: npm install react-simple-image-viewer
---
+++ client/src/pages/GoogleAuth.page.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+// redux
+import { useDispatch } from "react-redux";
+import { googleAuth } from "../redux/reducers/auth/auth.action";
+
+const GoogleAuth = () => {
+    const { token } = useParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (token) {
+            dispatch(googleAuth(token)).then(() => navigate("/delivery"));
+        }
+    }, [token]);
+
+    return <div>Loading, Please Wait...</div>;
+};
+
+export default GoogleAuth;
+
+// import React from "react";
+
+// const GoogleAuth = () => {
+//     return <div>GoogleAuth</div>;
+// };
+
+// export default GoogleAuth;
